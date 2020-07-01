cfffade4185f580f6db2c980c301715d006411ab
rafaroju89 <rafaroju89@gmail.com>
2020-07-01 12:54:29 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+require('./config/config');
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', function(req, res) {
+    res.send('get Usuario')
+})
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre es necesario'
+        });
+    } else {
+        res.send({ persona: body })
+    }
+})
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    res.json({
+        id,
+        usuario: 'post Usuario'
+    });
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto : ", process.env.PORT);
+});
