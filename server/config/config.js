eb3cd531d7fc4c646e3e4282a91c8430068944fd
rafaroju89 <rafaroju89@gmail.com>
2020-07-25 21:03:30 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -7,8 +7,6 @@ process.env.PORT = process.env.PORT || 3000;
 //Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-process.env.NODE_ENV = 'prod'
-
 //Base de datos
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
