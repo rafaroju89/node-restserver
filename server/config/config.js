804d9e0856f5c1827704ea263c1fe382cdb64eb5
rafaroju89 <rafaroju89@gmail.com>
2020-08-06 15:20:37 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://rafael:JhLvL8pBvKPbIVyK@cluster0.wdfvs.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 
